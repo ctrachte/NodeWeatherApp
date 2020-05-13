59c364f4a46ff66bb356a321c56126096ce116d4
ctrachte <cetrachte@hotmail.com>
2020-05-13 10:55:42 -0500

    add basic request format and header variables

--- app.js
+++ app.js
@@ -14,3 +14,18 @@
 
 // console.log('Stopping ...')
 
+const request = require('postman-request')
+const long = '-92.016712'
+const lat =  '34.957049'
+const access_key = '89cc5db32cd40ec478c4b603d3ec4460'
+
+let weatherUrl = `http://api.weatherstack.com/current?access_key=${access_key}&query=${lat},${long}`
+
+request({ url: weatherUrl }, (error, response) => {
+    if (error) {
+        console.log(error)
+    } else {
+        const data = JSON.parse(response.body)
+        console.log(data)
+    }
+})
