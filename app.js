1e06ff2c566cfefdfd623133fcf9e169f031f7d4
ctrachte <cetrachte@hotmail.com>
2020-05-13 10:29:24 -0500

    add npm and postman-request

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // Following tutorial from:
 // https://ngpec.udemy.com/course/the-complete-nodejs-developer-course-2/learn/lecture/13728910#overview
 
-console.log('Starting ...')
+// Lesson 6.31
+// console.log('Starting ...')
 
-setTimeout(() => {
-    console.log('2 Second Timer Popped!')
-}, 2000)
+// setTimeout(() => {
+//     console.log('2 Second Timer Popped!')
+// }, 2000)
 
-setTimeout(() => {
-    console.log('Immediate Timer Popped!')
-}, 0)
+// setTimeout(() => {
+//     console.log('Immediate Timer Popped!')
+// }, 0)
 
-console.log('Stopping ...')
+// console.log('Stopping ...')
+
