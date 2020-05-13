73c70e2d102d684eef1872cf83b399e30bb4a425
ctrachte <cetrachte@hotmail.com>
2020-05-13 14:50:33 -0500

    add request to mapbox for coordinates

--- app.js
+++ app.js
@@ -14,20 +14,38 @@
 
 // console.log('Stopping ...')
 
+
+// Lesson 6.32, WeatherStack API
 const request = require('postman-request')
 const long = '-92.016712'
 const lat =  '34.957049'
 const units = 'f'
-const access_key = '89cc5db32cd40ec478c4b603d3ec4460'
+const WeatherStackTokenID = '89cc5db32cd40ec478c4b603d3ec4460'
 
-let weatherUrl = `http://api.weatherstack.com/current?access_key=${access_key}&query=${lat},${long}&units=${units}`
+let weatherUrl = `http://api.weatherstack.com/current?access_key=${WeatherStackTokenID}&query=${lat},${long}&units=${units}`
 
-request({ url: weatherUrl, json: true }, (error, response) => {
+// request({ url: weatherUrl, json: true }, (error, response) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         let data = response.body.current
+//         let message = data.weather_descriptions[0] + ". It is currently " + data.temperature + "F but it feels like " + data.feelslike + "F."
+//         console.log(message)
+//     }
+// })
+
+// Lesson 6.33, Geocaching API
+// User input address -> get Lat/Long -> get weather
+const searchTerm = ""
+const MapBoxTokenID = "pk.eyJ1IjoiY3RyYWNodGUiLCJhIjoiY2pvYTlidHhjMGQxczNvbjBsMGNhaW95NCJ9.4ffU0qr3uXMd3kO8unKRTQ"
+const mapBoxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" + MapBoxTokenID + "&limit=1"
+
+request({ url: mapBoxURL, json: true }, (error, response) => {
     if (error) {
         console.log(error)
     } else {
-        let data = response.body.current
-        let message = data.weather_descriptions[0] + ". It is currently " + data.temperature + "F but it feels like " + data.feelslike + "F."
-        console.log(message)
+        lat = response.body.features[0].center[1]
+        long = response.body.features[0].center[0]
+        // console.log(data[0].center)
     }
 })
